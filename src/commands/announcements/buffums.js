14fcc10d0a3d05e9ce3b07c3352f305a5d489810
Linus Almgren <linus.almgren@gmail.com>
2024-06-16 01:30:18 +0200

    add timestamps to buffums command

--- src/commands/announcements/buffums.js
+++ src/commands/announcements/buffums.js
@@ -5,10 +5,18 @@ export default {
     .setName("buffums")
     .setDescription("Reminds people that buffs are coming")
     .addIntegerOption((option) =>
-      option.setName("combat").setDescription("Combat buffs in X min"),
+      option
+        .setName("combat")
+        .setDescription("Minutes combat buffs will be popped")
+        .setMinValue(0)
+        .setMaxValue(59),
     )
     .addIntegerOption((option) =>
-      option.setName("life").setDescription("Life buffs in X min"),
+      option
+        .setName("life")
+        .setDescription("Minutes life buffs will be popped")
+        .setMinValue(0)
+        .setMaxValue(59),
     ),
   async execute(interaction) {
     if (
@@ -22,14 +30,16 @@ export default {
       });
     }
 
-    const combat = interaction.options.getInteger("combat");
-    const life = interaction.options.getInteger("life");
+    const combat =
+      interaction.options.getInteger("combat") ?? getDefaultMinutes(0);
+    const life = interaction.options.getInteger("life") ?? getDefaultMinutes(2);
+
     const buffer =
       interaction.member.nickname ||
       interaction.user.globalName ||
       interaction.user.username;
 
-    const message = `# Guild Buffs incoming from ${buffer}! <@&1251296816260055080>\nCombat buffs will be popped ${combat} and life ${life}`;
+    const message = `### Guild Buffs incoming from ${buffer}! <@&1251296816260055080>\n${createTimeMsg(combat, life)}`;
 
     await interaction.guild.channels.cache
       .get("1162045473851441165")
@@ -37,4 +47,40 @@ export default {
 
     await interaction.reply({ content: "Buffums posted", ephemeral: true });
   },
+};
+
+const getDefaultMinutes = (offset) => {
+  const minutes = new Date().getMinutes() + 5 + offset;
+
+  if (minutes > 59) return minutes - 60;
+
+  return minutes;
+};
+
+const createTimeMsg = (combat, life) => {
+  const combatTime = getFutureUnixTimestamp(combat);
+  const lifeTime = getFutureUnixTimestamp(life);
+
+  if (combatTime < lifeTime) {
+    return `Combat will be popped <t:${combatTime}:R> and life <t:${lifeTime}:R>`;
+  }
+
+  return `Life will be popped <t:${lifeTime}:R> and combat <t:${combatTime}:R>`;
+};
+
+const getFutureUnixTimestamp = (targetMinutes) => {
+  const date = new Date();
+  date.setSeconds(0);
+  const minutes = date.getMinutes();
+
+  if (targetMinutes > minutes) {
+    date.setMinutes(targetMinutes);
+    return Math.floor(date.getTime() / 1000);
+  }
+
+  const difference = 60 + targetMinutes - minutes;
+
+  return Math.floor(
+    new Date(Date.now() + difference * 60 * 1000).getTime() / 1000,
+  );
 };
